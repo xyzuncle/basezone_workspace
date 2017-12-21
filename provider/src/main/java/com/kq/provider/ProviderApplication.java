2c4fc0f8f38e7dc59427ce5ece28169b0c7b907a
xyzuncle <w68335397@163.com>
2017-12-21 16:11:47 +0800

    测试feign是否具有负责均衡的能力

--- provider/src/main/java/com/kq/provider/ProviderApplication.java
+++ provider/src/main/java/com/kq/provider/ProviderApplication.java
@@ -1,14 +1,16 @@
 package com.kq.provider;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderApplication {
     public static void main(String[] args)
     {
-        new SpringApplicationBuilder(new Object[] { ProviderApplication.class }).web(true).run(args);
+        SpringApplication.run(ProviderApplication.class,args);
     }
 }
--- provider/src/main/java/com/kq/provider/controller/RefactorController.java
+++ provider/src/main/java/com/kq/provider/controller/RefactorController.java
@@ -1,16 +1,30 @@
 package com.kq.provider.controller;
 
 import com.kq.common.service.BaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.ribbon.LoadBalancerFeignClient;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
+/**idea properties 提示问题
  * 通过feign的好处就是controller不用写requestmapping
  * 真正的调用在common里的requestmapping
  */
 @RestController
 public class RefactorController implements BaseService {
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     public String say() {
-       return "hello new world.....";
+        ServiceInstance instance = this.discoveryClient.getLocalServiceInstance();
+
+       return "hello new world....."+"host:"+instance.getHost()+" port:"+instance.getPort();
     }
 }
