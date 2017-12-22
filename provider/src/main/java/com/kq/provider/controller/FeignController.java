5a54e266eac6ab61d0d6952b38b8ae3b62406079
xyzuncle <w68335397@163.com>
2017-12-22 12:00:23 +0800

    添加REST

--- provider/src/main/java/com/kq/provider/controller/FeignController.java
+++ provider/src/main/java/com/kq/provider/controller/FeignController.java
@@ -2,8 +2,10 @@ package com.kq.provider.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("feign")
+@RestController
 public class FeignController {
 
     @GetMapping("/say2")
