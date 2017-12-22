1d621e4129eecff8528e16c8889e2f2b58f1b6e7
xyzuncle <w68335397@163.com>
2017-12-22 14:02:43 +0800

    添加实现类

--- feignconsumer/src/main/java/com/kq/feign/service/ConsumerService.java
+++ feignconsumer/src/main/java/com/kq/feign/service/ConsumerService.java
@@ -3,6 +3,6 @@ package com.kq.feign.service;
 import com.kq.common.service.BaseService;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-
-public interface ConsumerService  {
+@FeignClient(value = "hello-service",fallback = FaceBackService.class)
+public interface ConsumerService extends FeignService {
 }
--- feignconsumer/src/main/java/com/kq/feign/service/FaceBackService2.java
+++ feignconsumer/src/main/java/com/kq/feign/service/FaceBackService2.java
@@ -1,18 +1,16 @@
 package com.kq.feign.service;
 
 
-import com.kq.common.service.BaseService;
-import feign.Feign;
 import org.springframework.stereotype.Component;
 
 /**
  * 要实现feign实现的接口
  */
 @Component
-public class FaceBackService implements FeignService{
+public class FaceBackService2 implements FeignService{
 
     @Override
     public String say2() {
-        return "服务未知。。";
+        return "我是服务未知2。。";
     }
 }
