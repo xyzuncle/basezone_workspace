b7ea4903682f24b1a81ce5041bd522c33a87c7aa
xyzuncle <w68335397@163.com>
2017-12-25 17:15:17 +0800

    配置网关的负载均衡能力

--- provider/src/main/java/com/kq/provider/controller/FeignController.java
+++ provider/src/main/java/com/kq/provider/controller/FeignController.java
@@ -1,5 +1,8 @@
 package com.kq.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FeignController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/say2")
     public String say2(){
-        return "i'm is say2";
+        ServiceInstance instance = this.discoveryClient.getLocalServiceInstance();
+        return "i'm is say2"+"host:"+instance.getHost()+" port:"+instance.getPort();
     }
 
 }
