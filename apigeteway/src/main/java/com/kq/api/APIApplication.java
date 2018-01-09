80cc90e5404c6a0f8cc86c0c8901d744082ef010
xyzuncle <w68335397@163.com>
2018-01-09 16:46:22 +0800

    添加服务发现配置文件

--- apigeteway/src/main/java/com/kq/api/APIApplication.java
+++ apigeteway/src/main/java/com/kq/api/APIApplication.java
@@ -2,12 +2,16 @@ package com.kq.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import java.rmi.activation.ActivationGroup;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@EnableOAuth2Sso
 public class APIApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIApplication.class, args);
