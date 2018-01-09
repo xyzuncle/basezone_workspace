b6b783477d7d6da42e69fda142c4e32cdc7b48c0
xyzuncle <w68335397@163.com>
2018-01-09 15:05:42 +0800

    测试网关反射的问题

--- oauth/src/main/java/com/kq/auth/config/WebSecurityConfig.java
+++ oauth/src/main/java/com/kq/auth/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -18,8 +19,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{//1
 	UserDetailsService customUserService(){ //2
 		return new CustomUserService();
 	}
-	
-	@Override
+
+
+
+    @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(customUserService()); //3
 		
@@ -32,25 +35,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{//1
 		return super.authenticationManagerBean();
 	}
 
-
-
-	
-	/*@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-						.anyRequest().authenticated() //4
-						.and()
-						.formLogin()
-							.loginPage("/login")
-							.failureUrl("/login?error")
-							.permitAll() //5
-						.and()
-						.logout().permitAll()
-                        .and()
-                        .csrf().disable(); //6
-		
-
-	}*/
+    /**
+     * 解决静态资源被拦截的问题
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //解决静态资源被拦截的问题
+        web.ignoring().antMatchers("/css/**");
+    }
 
 
 }
