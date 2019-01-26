63361a146b0577277a194a10577ada74ea03b5e3
xyzuncle <w68335397@163.com>
2019-01-26 13:38:46 +0800

    测试过滤器

--- register/src/main/java/com/kq/register/RegisterApplication.java
+++ register/src/main/java/com/kq/register/RegisterApplication.java
@@ -1,8 +1,10 @@
 package com.kq.register;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableEurekaServer
